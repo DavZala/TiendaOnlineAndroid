25a381938ba8ee19d2ea744eca52de3ce7dd1952
DavZala <zalasardavib@gmail.com>
2024-10-18 18:55:57 -0500

    Splash y Login

--- app/src/main/java/com/example/tiendaonline/Home.java
+++ app/src/main/java/com/example/tiendaonline/Home.java
@@ -1,9 +1,6 @@
 package com.example.tiendaonline;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginGeneral extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_general);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button inicioSesion = findViewById(R.id.btn_iniciarsesion);
-        inicioSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Crea un Intent para iniciar la segunda actividad
-                Intent intent = new Intent(LoginGeneral.this, Home.class);
-                startActivity(intent);
-            }
         });
     }
 }
