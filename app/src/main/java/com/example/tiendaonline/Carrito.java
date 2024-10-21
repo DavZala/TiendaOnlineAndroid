b41750b90950c54e8c5fc0bbcf08c161c0667ad1
DavZala <zalasardavib@gmail.com>
2024-10-21 15:16:21 -0500

    Ajustes

--- app/src/main/java/com/example/tiendaonline/Carrito.java
+++ app/src/main/java/com/example/tiendaonline/Carrito.java
@@ -1,6 +1,10 @@
 package com.example.tiendaonline;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,14 @@ public class Carrito extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageView volver = findViewById(R.id.btn_volver);
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
         });
     }
 }
--- app/src/main/java/com/example/tiendaonline/Registro.java
+++ app/src/main/java/com/example/tiendaonline/Registro.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,36 +12,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginGeneral extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login_general);
+        setContentView(R.layout.activity_registro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button inicioSesion = findViewById(R.id.btn_iniciarsesion);
-        inicioSesion.setOnClickListener(new View.OnClickListener() {
+        ImageView volver = findViewById(R.id.btn_volver);
+        volver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Crea un Intent para iniciar la segunda actividad
-                Intent intent = new Intent(LoginGeneral.this, Home.class);
-                startActivity(intent);
-            }
-        });
-
-        Button registro = findViewById(R.id.btn_registro);
-        registro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Crea un Intent para iniciar la segunda actividad
-                Intent intent = new Intent(LoginGeneral.this, Registro.class);
-                startActivity(intent);
+                finish();
             }
         });
     }
